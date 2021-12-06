78e82da066f4cadc757f13bc817516f579dc4fdd
David Hadas mac 16 <davidhadasmac16@users-MacBook-Pro.local>
2021-12-06 23:18:29 +0200

    initializing

--- queuegate/queuegate.go
+++ queuegate/queuegate.go
@@ -10,23 +10,31 @@ func GateHandler(logger *zap.SugaredLogger, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer h.ServeHTTP(w, r)
 
-		userAgent := r.Header.Get("userAgent")
-		contentEncoding := r.Header.Get("content-encoding")
-		transferEncoding := r.Header.Get("transfer-encoding")
-		keepAlive := r.Header.Get("keep-alive")
-		connection := r.Header.Get("connection")
-		xForwardedFor := r.Header.Get("x-forwarded-for")
-		cacheControl := r.Header.Get("cache-control")
-		via := r.Header.Get("via")
+		logger.Info("DH> list headers ")
+		for name, values := range r.Header {
+			// Loop over all values for the name.
+			for _, value := range values {
+				logger.Info(name, value)
+			}
+		}
+		/*
+			userAgent := r.Header.Get("User-Agent")
+			contentEncoding := r.Header.Get("content-encoding")
+			transferEncoding := r.Header.Get("transfer-encoding")
+			keepAlive := r.Header.Get("keep-alive")
+			connection := r.Header.Get("Connection")
+			xForwardedFor := r.Header.Get("x-forwarded-for")
+			cacheControl := r.Header.Get("cache-control")
+			via := r.Header.Get("via")
 
-		logger.Info("DH> userAgent ", userAgent)
-		logger.Info("DH> contentEncoding ", contentEncoding)
-		logger.Info("DH> transferEncoding ", transferEncoding)
-		logger.Info("DH> keepAlive ", keepAlive)
-		logger.Info("DH> connection ", connection)
-		logger.Info("DH> xForwardedFor ", xForwardedFor)
-		logger.Info("DH> cacheControl ", cacheControl)
-		logger.Info("DH> via ", via)
-
+			logger.Info("DH> userAgent ", userAgent)
+			logger.Info("DH> contentEncoding ", contentEncoding)
+			logger.Info("DH> transferEncoding ", transferEncoding)
+			logger.Info("DH> keepAlive ", keepAlive)
+			logger.Info("DH> connection ", connection)
+			logger.Info("DH> xForwardedFor ", xForwardedFor)
+			logger.Info("DH> cacheControl ", cacheControl)
+			logger.Info("DH> via ", via)
+		*/
 	})
 }
